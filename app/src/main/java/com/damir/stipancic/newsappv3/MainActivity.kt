9ac52a953ed222a10dc2ae386e1e82439adbc7b0
DamirStipa <stipa.damir@hotmail.com>
2022-07-11 12:57:14 +0200

    Initial project commit

---
+++ app/src/main/java/com/damir/stipancic/newsappv3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.damir.stipancic.newsappv3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
